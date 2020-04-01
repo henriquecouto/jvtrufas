b0626d9cabbde45bb6ca35eab020505b9139f793
Henrique Couto <henrique.coutu@live.com>
2020-04-01 20:45:56 -0300

    fix import from ItemRouter

--- packages/api/src/routes/AdminRouter/index.js
+++ packages/api/src/routes/AdminRouter/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const authMiddleware = require("../../middlewares/authMiddleware");
 
 router.use(authMiddleware("admin"));
-router.use("/product", require("./ProductRouter"));
+router.use("/product", require("./ItemRouter"));
 
 module.exports = router;
--- packages/api/src/routes/PurchaserRouter/index.js
+++ packages/api/src/routes/PurchaserRouter/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const authMiddleware = require("../../middlewares/authMiddleware");
 
 router.use(authMiddleware("purchaser"));
-
-router.use("/product", require("./ProductRouter"));
+router.use("/product", require("./ItemRouter"));
 
 module.exports = router;
