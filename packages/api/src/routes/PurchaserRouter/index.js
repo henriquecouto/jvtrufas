34f8772bbad7969cbdb82187b4b5279e08faf4e6
Henrique Couto <henrique.coutu@live.com>
2020-07-04 15:19:47 -0300

    init address router

--- packages/api/src/routes/PurchaserRouter/index.js
+++ packages/api/src/routes/PurchaserRouter/index.js
@@ -4,5 +4,6 @@ const authMiddleware = require("../../middlewares/authMiddleware");
 router.use(authMiddleware("purchaser"));
 router.use("/product", require("./ItemRouter"));
 router.use("/order", require("./OrderRouter"));
+// router.use("/address", require("./AddressRouter"));
 
 module.exports = router;
