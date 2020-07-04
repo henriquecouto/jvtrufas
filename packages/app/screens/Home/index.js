75bd958590deb672e386d9d9470daf381c6944b0
Henrique Couto <henrique.coutu@live.com>
2020-07-04 18:21:34 -0300

    init home screen

--- packages/app/screens/Home/index.js
+++ packages/app/screens/Home/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 
 export default function Home() {
   return (
-    <View>
-      <Text style={styles.text}>JV Trufas</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.text}>JV Trufas</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    padding: 20,
+  },
   text: {
     fontSize: 50,
     fontFamily: 'FredokaOne-Regular',
+    color: '#5c2f0c',
   },
 });
