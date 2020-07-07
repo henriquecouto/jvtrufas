922890ddbe367465ec74467ef119ec0b76e0bf7f
Henrique Couto <henrique.coutu@live.com>
2020-07-07 13:48:43 -0300

    app init flatlist to show orders ins home

--- packages/app/src/screens/Home/index.js
+++ packages/app/src/screens/Home/index.js
@@ -3,39 +3,48 @@ import {ScrollView, View, StyleSheet, Text} from 'react-native';
 import Header from '../../components/Header';
 import LargeButton from '../../components/LargeButton';
 import {GlobalContext} from '../../contexts/global';
+import {FlatList} from 'react-native-gesture-handler';
 
 export default function Home({navigation}) {
   const [{cart, orders}] = useContext(GlobalContext);
 
   return (
-    <ScrollView>
-      <View style={styles.root}>
-        <Header title="JV Trufas" />
-        <LargeButton
-          header="Fazer encomenda"
-          title="Deliciosas trufas e licores esperam por você! 😉"
-          subtitle="Eu quero"
-          onPress={() => navigation.navigate('Shop')}
-        />
-        {cart.items && (
+    <FlatList
+      contentContainerStyle={styles.root}
+      ListHeaderComponent={() => (
+        <View>
+          <Header title="JV Trufas" />
           <LargeButton
-            header="Seu carrinho 🛒"
-            title={`Você tem ${cart.items.length} ${
-              cart.items.length > 1 ? 'itens prontos' : 'item pronto'
-            } para pedir`}
-            subtitle="Ver agora"
-            onPress={() => navigation.navigate('CartHome')}
+            header="Fazer encomenda"
+            title="Deliciosas trufas e licores esperam por você! 😉"
+            subtitle="Eu quero"
+            onPress={() => navigation.navigate('Shop')}
           />
-        )}
-        <Text>{orders.length}</Text>
-      </View>
-    </ScrollView>
+          {cart.items && (
+            <LargeButton
+              header="Seu carrinho 🛒"
+              title={`Você tem ${cart.items.length} ${
+                cart.items.length > 1 ? 'itens prontos' : 'item pronto'
+              } para pedir`}
+              subtitle="Ver agora"
+              onPress={() => navigation.navigate('CartHome')}
+            />
+          )}
+        </View>
+      )}
+      data={orders}
+      renderItem={({item}) => {
+        console.log(item);
+        return <Text>{JSON.stringify(item)}</Text>;
+      }}
+      keyExtractor={(item) => item._id}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   root: {
-    height: '100%',
+    // height: '100%',
     padding: 20,
     justifyContent: 'space-evenly',
   },
