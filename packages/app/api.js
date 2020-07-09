5c05c95d55d4f06f2f31e35df1581e5c23a9f2cb
Henrique Couto <henrique.coutu@live.com>
2020-07-08 23:47:35 -0300

    fix path

--- packages/app/api.js
+++ packages/app/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'http://192.168.3.109:3030';
+export const baseURL = 'https://jvtrufas.henriquecouto.com.br';
 
 export default axios.create({
   baseURL,
