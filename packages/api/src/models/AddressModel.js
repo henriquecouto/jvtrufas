b11d8ce52d35c039121a0b48b1423ef6e2a5b599
Henrique Couto <henrique.coutu@live.com>
2020-08-09 17:38:18 -0300

    #3 - increase limit of address attributes

--- packages/api/src/models/AddressModel.js
+++ packages/api/src/models/AddressModel.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const AddressSchema = new mongoose.Schema({
-  street: { type: String, required: true, maxlength: 50, minlength: 5 },
-  neighborhood: { type: String, required: true, maxlength: 20 },
-  landmark: { type: String, required: true, maxlength: 30 },
+  street: { type: String, required: true, maxlength: 100 },
+  neighborhood: { type: String, required: true, maxlength: 50 },
+  landmark: { type: String, required: true, maxlength: 50 },
 });
 
 module.exports = {
