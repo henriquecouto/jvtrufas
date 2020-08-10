d47c5e19a4cb0887b9e03fe4b7e53a38437fc42d
Henrique Couto <henrique.coutu@live.com>
2020-08-10 20:17:38 -0300

    release 1.3.1

--- packages/adm-web/src/api.js
+++ packages/adm-web/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "https://jvtrufas.henriquecouto.com.br";
-// export const baseURL = "http://192.168.3.110:3030";
+// export const baseURL = "https://jvtrufas.henriquecouto.com.br";
+export const baseURL = "http://192.168.3.110:3030";
 
 export default axios.create({
   baseURL,
